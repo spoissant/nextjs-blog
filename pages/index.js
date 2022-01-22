941c9dd623f2b4b52d659815b5200eb6d0b8555e
spoissant <simon.poissant@hivebrite.com>
2022-01-22 08:26:20 -0500

    Test dev

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Simon is a fullstack developer with emphasis on frontend development.</p>
+        <p>Simon is a Tyranid Prime in disguise.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
